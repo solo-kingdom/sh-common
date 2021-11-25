d37fb19afdbc0d64e72b51b0a8a3c8e90fdce932
sunzhenkai <zhenkai.sun@qq.com>
2021-11-25 12:27:49 +0800

    add tools.sh

---
+++ tools.sh
@@ -0,0 +1,12 @@
+info() {
+  printf "%s - [INFO] %s\n" "$(date '+%Y-%m-%d %H:%M:%S')" "$*" >&2
+}
+
+error() {
+  printf "%s - [ERROR] %s\n" "$(date '+%Y-%m-%d %H:%M:%S')" "$*" >&2
+}
+
+quit() {
+  echo "$1"
+}
+
